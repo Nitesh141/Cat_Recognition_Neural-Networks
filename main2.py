5d03ff09108d0ac41f9112778bf97ae8a88aff18
Nitesh Kumar <nk14@iitbbs.ac.in>
2018-03-22 11:27:08 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,68 @@
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+from dnn_app_utils_v3 import *
+from nn_module import *
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+np.random.seed(1)
+
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+
+# Explore your dataset
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(train_x_orig.shape))
+print ("train_y shape: " + str(train_y.shape))
+print ("test_x_orig shape: " + str(test_x_orig.shape))
+print ("test_y shape: " + str(test_y.shape))
+
+
+# Reshape the training and test examples
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# Standardize data to have feature values between 0 and 1.
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
+
+
+### CONSTANTS DEFINING THE MODEL ####
+n_x = 12288     # num_px * num_px * 3
+n_h = 7
+n_y = 1
+layers_dims = (n_x, n_h, n_y)
+
+
+
+
+
+predictions_train = predict(train_x, train_y, parameters)
+
+predictions_test = predict(test_x, test_y, parameters)
+
+### CONSTANTS ###
+layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
+
+pred_train = predict(train_x, train_y, parameters)
+
+
+pred_test = predict(test_x, test_y, parameters)
